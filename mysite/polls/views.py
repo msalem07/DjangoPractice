3e79a998544bfb49e3ceb570560a66f988726754
Mariano S <msalem_deeap0810@yahoo.com>
2018-03-21 23:58:35 -0500

    Update results page to show answers

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -36,8 +36,8 @@ def detail(request, question_id):
     return render(request, 'polls/details.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question {}".format(question_id)
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
