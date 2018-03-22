02db3509a31e7b2d6328b8bca63746b849417548
Mariano S <msalem_deeap0810@yahoo.com>
2018-03-21 22:56:26 -0500

    Modified commented shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,12 +13,15 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #output = ', '.join([q.question_text for q in latest_question_list])
     
-    #without shortcut
-    #template = loader.get_template('polls/index.html')
+    
 
     context = {
         'latest_question_list': latest_question_list,
     }
+    #without shortcut
+    #template = loader.get_template('polls/index.html')
+    #return HttpResponse(template.render(context, request))
+    
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
