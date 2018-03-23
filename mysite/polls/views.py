e854f79815406430f3081be94b445906b8545b03
Mariano S <msalem_deeap0810@yahoo.com>
2018-03-22 22:32:48 -0500

    Use generic templates to return data

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,41 +3,60 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Question, Choice
-
+from django.views import generic
 #shorcut to loader.get_template
 from django.shortcuts import render
 
-
 # Create your views here.
+#Initial Templates, these are being replaced with the bottom ones 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     #output = ', '.join([q.question_text for q in latest_question_list])
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     #without shortcut
+#     #template = loader.get_template('polls/index.html')
+#     #return HttpResponse(template.render(context, request))
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    #without shortcut
-    #template = loader.get_template('polls/index.html')
-    #return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html', context)
 
-    return render(request, 'polls/index.html', context)
+# def detail(request, question_id):
 
-def detail(request, question_id):
+#     #Without shortcut
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exists")
 
-    #Without shortcut
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exists")
+#     #With shortcut
+#     question = get_object_or_404(Question, pk=question_id)
 
-    #With shortcut
-    question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/details.html', {'question': question})
 
-    return render(request, 'polls/details.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class IndexView(generic.ListView):
+
+    #The context attribute for listView returned becomes the models name in lowercase plus list, in this case question_list. 
+    template_name = 'polls/index.html
+    #Override that name and use custom one.'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    #The context attribute returned becomes the models name in lowercase, in this case question.
+    #Argument on url needs to now refer to pk instead of question_id
+    model = Question
+    template_name = 'polls/details.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
